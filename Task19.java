b086245d444585ec5f27b804cbc5a45c61dadf8a
naveena52 <naveena52.ponna@gmail.com>
2022-07-20 14:52:46 +0530

    Given number is Armstrong or not

---
+++ Task19.java
@@ -0,0 +1,42 @@
+// java program to display given number is armstrong or not//
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Task19
+{
+    static boolean isArmstrong(int n)
+    {
+        int temp, digits=0, last=0, sum=0;
+        temp=n;
+        while(temp>0)
+        {
+            temp = temp/10;
+            digits++;
+        }
+        temp = n;
+        while(temp>0)
+        {
+            last = temp % 10;
+            sum +=  (Math.pow(last, digits));
+            temp = temp/10;
+        }
+        if(n==sum)
+            return true;
+        else return false;
+    }
+    public static void  main(String args[])
+    {
+        int num;
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        num=sc.nextInt();
+        if(isArmstrong(num))
+        {
+            System.out.print("The given number is a Armstrong ");
+        }
+        else
+        {
+            System.out.print(" The given number is Not Armstrong ");
+        }
+    }
+}
